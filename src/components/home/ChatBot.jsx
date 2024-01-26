b268653c355438671d9fbcf76ddd9cd4ba9bbcaf
Lorenzo Caballero <zeldiuss@gmail.com>
2024-01-26 00:42:39 -0300

    ia chat added

--- src/components/home/ChatBot.jsx
+++ src/components/home/ChatBot.jsx
@@ -1,10 +1,7 @@
 import React, { useState, useRef } from 'react';
 import axios from 'axios';
-import { FiMessageSquare } from 'react-icons/fi'; 
+import { FiMessageSquare, FiX } from 'react-icons/fi'; // Importa el icono de cierre
 import { motion } from "framer-motion";
-import {config} from "dotenv"
-
-config()
 
 const apiUrl = 'https://api.openai.com/v1/chat/completions';
 
@@ -12,13 +9,13 @@ const ChatButton = () => {
   const [chatAbierto, setChatAbierto] = useState(false);
   const [mensajes, setMensajes] = useState([]);
   const [nuevoMensaje, setNuevoMensaje] = useState('');
-  const [escribiendo, setEscribiendo] = useState(false); // Estado para controlar la animación de escribiendo
+  const [escribiendo, setEscribiendo] = useState(false);
   const chatRef = useRef(null);
 
   const obtenerRespuestaFauno = async (userMessage) => {
-    const apikey =  process.env.API_KEY; // Agrega tu API Key de OpenAI
+    const apikey = process.env.REACT_APP_OPENAI_API_KEY;
     try {
-      setEscribiendo(true); // Activar la animación de escribiendo
+      setEscribiendo(true);
       const respuesta = await axios.post(
         apiUrl,
         {
@@ -27,7 +24,7 @@ const ChatButton = () => {
             {"role": "user", "content": userMessage} 
           ],
           model: "gpt-3.5-turbo",
-          max_tokens: 140,
+          max_tokens: 100,
         },
         {
           headers: {
@@ -37,19 +34,19 @@ const ChatButton = () => {
         }
       );
 
-      setEscribiendo(false); // Desactivar la animación de escribiendo
+      setEscribiendo(false);
       return respuesta.data.choices[0].message.content;
     } catch (error) {
       console.error('Error al llamar a la API de OpenAI:', error);
-      setEscribiendo(false); // Desactivar la animación de escribiendo en caso de error
+      setEscribiendo(false);
       return 'Lo siento, hubo un problema al procesar tu solicitud.';
     }
   };
 
   const handleEnviarMensaje = async () => {
-    if (!nuevoMensaje.trim()) return; // Evitar enviar mensajes vacíos
+    if (!nuevoMensaje.trim()) return;
     setMensajes((prevMensajes) => [...prevMensajes, { texto: nuevoMensaje, origen: 'usuario' }]);
-    setNuevoMensaje(''); // Limpiar el input
+    setNuevoMensaje('');
     const respuestaFauno = await obtenerRespuestaFauno(nuevoMensaje);
     setMensajes((prevMensajes) => [...prevMensajes, { texto: respuestaFauno, origen: 'fauno' }]);
   };
@@ -64,6 +61,10 @@ const ChatButton = () => {
     setChatAbierto(!chatAbierto);
   };
 
+  const handleCloseChat = () => {
+    setChatAbierto(false);
+  };
+
   return (
     <div className="fixed bottom-0 right-4">
       <button onClick={handleChatToggle} className={`flex items-center justify-center bg-purple-300 rounded-full w-12 h-12 ${chatAbierto ? 'hidden' : ''}`}>
@@ -71,22 +72,25 @@ const ChatButton = () => {
       </button>
       {chatAbierto && (
         <div className="bg-purple-100 p-4 rounded-t-lg shadow-lg w-80">
+          <div className="flex justify-between mb-2">
+            <button onClick={handleCloseChat}><FiX className="text-gray-600" /></button> {/* Botón de cierre */}
+          </div>
           <div className="h-60 overflow-y-auto mb-2">
             {mensajes.map((mensaje, index) => (
               <div key={index} className={`mb-2 ${mensaje.origen === 'usuario' ? 'text-right' : 'text-left'} px-4 py-2 rounded-lg bg-purple-200 text-gray-800`}>
                 <strong>{mensaje.origen === 'usuario' ? 'Tú' : 'Asistente Fauno'}</strong>: {mensaje.texto}
               </div>
             ))}
-        {escribiendo && (
-  <motion.div
-    initial={{ opacity: 0 }} // Opacidad inicial
-    animate={{ opacity: 1 }} // Opacidad al animar
-    transition={{ duration: 0.5 }} // Duración de la animación
-    className="mb-2 text-left px-4 py-2 rounded-lg bg-purple-200"
-  >
-    <strong>Asistente Fauno</strong>: ...
-  </motion.div>
-)}
+            {escribiendo && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                className="mb-2 text-left px-4 py-2 rounded-lg bg-purple-200"
+              >
+                <strong>Asistente Fauno</strong>: ...
+              </motion.div>
+            )}
           </div>
           <div className="flex justify-between">
             <input
